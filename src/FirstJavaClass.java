b56ba9721beec8a8d1f099e87babec9abc43d573
navsidhu6 <57518878+navsidhu6@users.noreply.github.com>
2020-05-20 18:36:43 -0700

    Initial Commit : Java Class

---
+++ src/FirstJavaClass.java
@@ -0,0 +1,12 @@
+public class FirstJavaClass {
+    public static void main(String[] args) {
+        int firstVariable = 10;
+        int secondVariable = 20;
+        int total = firstVariable + secondVariable;
+        System.out.println("First Variable :" + firstVariable);
+        System.out.println("Second Variable :" + secondVariable);
+        System.out.println("Total =" + total);
+
+
+    }
+}
